9bee99530bee3a9fa6f3e2ec3679430daee6345e
ari-fatur <ari.arifatur.fatur@gmail.com>
2024-05-03 07:34:37 +0700

    init

---
+++ src/main.rs
@@ -0,0 +1,28 @@
+use axum::Router;
+use axum::routing::get;
+use reqwest::Client;
+use crate::reverse::get_image_linksaya;
+
+mod reverse;
+
+#[tokio::main]
+async fn main() {
+
+   server().await
+}
+
+async fn server(){
+    let client  =Client::new();
+
+    let port = dotenvy::var("PORT").unwrap_or("3000".to_string());
+    let app = Router::new()
+        .route("/linksaya", get(get_image_linksaya))
+        .with_state(client);
+
+    // run our app with hyper, listening globally on port 3000
+    let addr = format!("0.0.0.0:{}", port);
+    println!("server run -> {}", addr);
+    let listener = tokio::net::TcpListener::bind(addr).await.unwrap();
+    axum::serve(listener, app).await.unwrap();
+}
+
